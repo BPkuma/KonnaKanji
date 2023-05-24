89d3e65eb272e0004244fbe4d5fb5d1c31e86766
BPkuma <omomotakuma@gmail.com>
2023-05-24 09:02:02 +0900

    add:リロードすると設問分と選択肢のセットがランダムで入れ替わる機能

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // result表示する画像の配置位置を定義
-const RESULT_FIELD = [
+const resultPosition = [
   {
     position : "left",
     pos_code : 0,
@@ -16,65 +16,98 @@ const RESULT_FIELD = [
   },
 ];
 
-const nowFeeling = [
+// 設問文と答えの組み合わせを定義
+const matters = [
   {
     id: "01",
     question: "今のあなたは",
-    answer01: "明るい。",
-    answer02: "暗い。",
+    answerA: "明るい。",
+    answerB: "暗い。",
   },
   {
     id: "02",
     question: "今食べるなら",
-    answer01: "肉。",
-    answer02: "魚。",
+    answerA: "肉。",
+    answerB: "魚。",
   },
   {
     id: "03",
     question: "今戯れるなら",
-    answer01: "犬。",
-    answer02: "猫。",
+    answerA: "犬。",
+    answerB: "猫。",
   },
   {
     id: "04",
     question: "どちらか言えば",
-    answer01: "誰かと話したい。",
-    answer02: "一人でいたい。",
+    answerA: "誰かと話したい。",
+    answerB: "一人でいたい。",
   },
   {
     id: "05",
     question: "今の自分を天気で言うと",
-    answer01: "晴れ。",
-    answer02: "曇り。",
+    answerA: "晴れ。",
+    answerB: "曇り。",
   },
   {
     id: "06",
     question: "自然と足が向きそうなのは",
-    answer01: "森。",
-    answer02: "海。",
+    answerA: "森。",
+    answerB: "海。",
   },
   {
     id: "07",
     question: "感情の波が",
-    answer01: "激しい。",
-    answer02: "凪いでいる。",
+    answerA: "激しい。",
+    answerB: "凪いでいる。",
   },
   {
     id: "08",
     question: "今の気分の服装は",
-    answer01: "ミニマル。",
-    answer02: "派手。",
+    answerA: "ミニマル。",
+    answerB: "派手。",
   },
   {
     id: "09",
     question: "容姿のコンディションは",
-    answer01: "かわいい。",
-    answer02: "かっこいい。",
+    answerA: "かわいい。",
+    answerB: "かっこいい。",
   },
   {
     id: "10",
     question: "今眠りに就くなら",
-    answer01: "ハンモックの上。",
-    answer02: "布団の中。",
+    answerA: "ハンモックの上。",
+    answerB: "布団の中。",
   },
 ];
+
+const answerArray = [];
+
+const question = document.getElementById('question');
+const answerA = document.querySelector('.answerA');
+const answerB = document.querySelector('.answerB');
+const resultField = document.getElementById('resultField');
+const result01 = document.querySelector('.result01');
+const result02 = document.querySelector('.result02');
+
+function setQuestion() {
+  let rand = Math.floor(Math.random() * matters.length);
+  let matter = matters.splice(rand, 1)[0];
+
+  question.textContent = matter['question'];
+  answerA.textContent = matter['answerA'];
+  answerB.textContent = matter['answerB'];
+}
+
+function setResult(e) {
+
+}
+
+setQuestion();
+
+answerA.addEventListener('click', (e) => {
+  
+});
+
+answerB.addEventListener('click', (e) => {
+  
+});
